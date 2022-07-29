cf27f683214d600ca0a985dd6494488cd556d539
Jordan Wong <pjwong97@gmail.com>
2022-07-29 16:04:21 -0700

    complete JS to put board in DOM

--- static/boggle.js
+++ static/boggle.js
@@ -22,9 +22,25 @@ async function start() {
 /** Display board */
 
 function displayBoard(board) {
-  // $table.empty();
+  $table.empty();
   // loop over board and create the DOM tr/td structure
+  //make table body
+  const $body = $('<tbody>');
+  for (let y = 0; y < board.length; y++) {
+    //make table row
+    const $row = $('<tr>');
+    for (let x = 0; x < board.length; x++) {
+      //make table data with td filled with letters from board
+      //append to rows
+      const $letter = $('<td>').text(board[y][x]);
+      $row.append($letter);
+    }
+    //append rows to body
+    $body.append($row);
+  }
+  $table.append($body);
 }
 
 
 start();
+
